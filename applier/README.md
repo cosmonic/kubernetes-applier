90abfa96deef846a0d97617cdeb739fbc0fb0d0b
Taylor Thomas <taylor@cosmonic.com>
2021-12-09 17:37:43 -0700

    Feat/working provider (#1)
    * Compiling, but untested provider
    
    * Working applier with happy path test
    
    * Adds tests for everything
    
    * Add GH workflow
    
    * Updates readme with useful info
    
    * Address PR feedback

--- applier/README.md
+++ applier/README.md
@@ -1,8 +1,59 @@
-# applier capability provider
+# Kubernetes Applier Capability Provider
 
-This capability provider 
-implements the "wasmcloud:example:factorial" capability
-and calculates "n factorial" for the provided whole number n.
+This is a capability provider implementation of the `cosmonic:kubernetes_applier` contract. Its
+purpose is to take arbitrary manifests from an actor and do the equivalent of a `kubectl apply` to
+create the object.
 
-Build with 'make'. Test with 'make test'.
+## Using the provider
 
+TODO: Put in the OCI reference here once we push
+
+The only configuration required for linking to this provider is a valid kubeconfig. There are 3 ways
+of doing this:
+
+- The default (if no config is specified) will attempt to infer the kubeconfig from the default
+  location (e.g. `$HOME/.kube/config`) or, if it is running in a pod, from the environment variables
+  in the pod. This option is great for local testing and for running this provider within a host
+  running in a pod.
+- The `config_b64` key: The value of this key should be the base64 encoded kubeconfig the provider
+  should use. Please note that this kubeconfig should have all certs and tokens embedded within the
+  kubeconfig (i.e. `client-certificate-data`). If any file paths are used, the link will be
+  rejected.
+- The `config_file` key: A specific path where the kubeconfig should be loaded from. This option
+  does allow file paths and is recommended when you have full control over the host and are storing
+  the kubeconfig in a location other than the default
+
+## Contributing
+
+We welcome all contributions! If you would like to submit changes, please open a [Pull
+Request](https://github.com/cosmonic/kubernetes-applier/pulls) and one of the maintainers will
+review it
+
+### Prerequisites
+
+In order to build this module, you will need to have the following tools installed:
+
+- `make`
+- [`wash`](https://wasmcloud.dev/overview/installation/#install-wash)
+- `jq`
+
+### Building
+
+To build the binary, simply run `make build`. To build and sign the provider for use with a
+wasmCloud host, run `make`
+
+### Testing
+
+Before running the test, you need to have a valid kubeconfig pointing at a running Kubernetes
+cluster (we recommend using [kind](https://kind.sigs.k8s.io/)).
+
+For ease of testing, we use NATS in a docker image. The tests can be run manually by running `cargo
+test --tests` if you wish to setup your own NATS server. Otherwise, you can just run `make test` to
+run all tests.
+
+#### Troubleshooting
+
+For maximum compatibility, we use rustls for the TLS stack. However, this can cause issues with
+kubeconfigs that contain a server IP address rather than a FQDN (such as those created by `kind`).
+If you see an error about an unrecognized domain name, make sure the server entry in your kubeconfig
+is using a domain name (e.g. switching `127.0.0.1` to `localhost`).
--- applier/tests/applier_test.rs
+++ applier/tests/applier_test.rs
@@ -1,3 +1,5 @@
+use k8s_openapi::api::core::v1::Service;
+use kube::{api::PostParams, Api};
 use kubernetes_applier_interface::*;
 use wasmbus_rpc::provider::prelude::*;
 use wasmcloud_test_util::{
@@ -12,14 +14,21 @@ use wasmcloud_test_util::{run_selected, run_selected_spawn};
 #[tokio::test]
 async fn run_all() {
     let opts = TestOptions::default();
-    let res = run_selected_spawn!(&opts, health_check, factorial_0_1);
+    let res = run_selected_spawn!(
+        &opts,
+        health_check,
+        create_update_delete_happy_path,
+        invalid_create,
+        invalid_update,
+        nonexistent_delete
+    );
     print_test_results(&res);
 
     let passed = res.iter().filter(|tr| tr.passed).count();
     let total = res.len();
     assert_eq!(passed, total, "{} passed out of {}", passed, total);
 
-    // try to let the provider shut dowwn gracefully
+    // try to let the provider shut down gracefully
     let provider = test_provider().await;
     let _ = provider.shutdown().await;
 }
@@ -34,21 +43,224 @@ async fn health_check(_opt: &TestOptions) -> RpcResult<()> {
     Ok(())
 }
 
-/// tests of the Factorial capability
-async fn factorial_0_1(_opt: &TestOptions) -> RpcResult<()> {
-    let prov = test_provider().await;
+const VALID_MANIFEST: &str = r#"apiVersion: v1
+kind: Service
+metadata:
+  name: foo-applier-test-happy
+  labels:
+    wasmcloud.dev/test: "true"
+spec:
+  selector:
+    app.kubernetes.io/name: foo-applier
+  ports:
+    - protocol: TCP
+      port: 8080
+      targetPort: 8080"#;
 
+const VALID_MANIFEST_WITH_LABELS: &str = r#"apiVersion: v1
+kind: Service
+metadata:
+  name: foo-applier-test-happy
+  labels:
+    wasmcloud.dev/test: "true"
+    foo: happy
+spec:
+  selector:
+    app.kubernetes.io/name: foo-applier
+  ports:
+    - protocol: TCP
+      port: 8080
+      targetPort: 8080"#;
+
+/// Test the happy path of creating updating and deleting
+async fn create_update_delete_happy_path(_opt: &TestOptions) -> RpcResult<()> {
+    let prov = test_provider().await;
+    let svc_name = "foo-applier-test-happy";
+
+    let client = kube::Client::try_default()
+        .await
+        .expect("Unable to get client");
+    let api: Api<Service> = Api::default_namespaced(client);
+
+    // The test scaffolding doesn't wait for an ack from the link, so wait for a bit
+    tokio::time::sleep(std::time::Duration::from_secs(3)).await;
+
+    let actor_id = prov.origin().public_key();
     // create client and ctx
     let client = KubernetesApplierSender::via(prov);
-    let ctx = Context::default();
+    let ctx = Context {
+        actor: Some(actor_id),
+        ..Default::default()
+    };
 
-    // let resp = client.calculate(&ctx, &0).await?;
-    // assert_eq!(resp, 1, "0!");
+    let resp = client
+        .apply(&ctx, &VALID_MANIFEST.as_bytes().to_vec())
+        .await?;
+    assert!(resp.succeeded, "Create should have succeeded");
 
-    // let resp = client.calculate(&ctx, &1).await?;
-    // assert_eq!(resp, 1, "1!");
+    // Validate service exists
+    api.get(svc_name)
+        .await
+        .unwrap_or_else(|_| panic!("Service {} does not exist", svc_name));
 
-    // TODO: tests
+    let resp = client
+        .apply(&ctx, &VALID_MANIFEST_WITH_LABELS.as_bytes().to_vec())
+        .await?;
+    assert!(resp.succeeded, "Update should have succeeded");
+
+    let svc = api
+        .get(svc_name)
+        .await
+        .unwrap_or_else(|_| panic!("Service {} does not exist", svc_name));
+
+    assert_eq!(
+        svc.metadata
+            .labels
+            .expect("Should have labels present")
+            .get("foo")
+            .expect("foo label doesn't exist"),
+        "happy",
+        "Label value should be set correctly"
+    );
+
+    let resp = client
+        .delete(
+            &ctx,
+            &DeleteRequest {
+                group: String::new(),
+                kind: "Service".into(),
+                version: "v1".into(),
+                name: svc_name.into(),
+                ..Default::default()
+            },
+        )
+        .await?;
+    assert!(resp.succeeded, "Delete should have succeeded");
+    if api.get(svc_name).await.is_ok() {
+        panic!("Service {} should be deleted", svc_name)
+    }
+    Ok(())
+}
+
+// TODO: Test base64 config and file path config once https://github.com/wasmCloud/wasmcloud-test/issues/6 is fixed
+
+const INVALID_MANIFEST: &str = r#"apiVersion: v1
+kind: NotReal
+metadata:
+  name: foo-applier-test-invalid
+  labels:
+    wasmcloud.dev/test: "true"
+spec:
+  selector:
+    app.kubernetes.io/name: foo-applier
+  ports:
+    - protocol: TCP
+      port: 8080
+      targetPort: 8080
+  totallyNotValid: bar"#;
+
+/// Test that an invalid create fails
+async fn invalid_create(_opt: &TestOptions) -> RpcResult<()> {
+    let prov = test_provider().await;
+    // The test scaffolding doesn't wait for an ack from the link, so wait for a bit
+    tokio::time::sleep(std::time::Duration::from_secs(3)).await;
+
+    let actor_id = prov.origin().public_key();
+    // create client and ctx
+    let client = KubernetesApplierSender::via(prov);
+    let ctx = Context {
+        actor: Some(actor_id),
+        ..Default::default()
+    };
+
+    let resp = client
+        .apply(&ctx, &INVALID_MANIFEST.as_bytes().to_vec())
+        .await?;
+    assert!(!resp.succeeded, "Create should not have succeeded");
+    assert!(resp.error.is_some(), "Error message should be set");
+
+    Ok(())
+}
+
+const INVALID_UPDATE_MANIFEST: &str = r#"apiVersion: v1
+kind: Service
+metadata:
+  name: foo-applier-test-happy
+  labels:
+    wasmcloud.dev/test: "true"
+spec:
+  selector:
+    app.kubernetes.io/name: foo-applier
+  ports:
+    - protocol: TCP
+      port: 8080
+      targetPort: 8080
+      totallyNotValid: bar"#;
+
+/// Test that an invalid update fails
+async fn invalid_update(_opt: &TestOptions) -> RpcResult<()> {
+    let prov = test_provider().await;
+
+    let client = kube::Client::try_default()
+        .await
+        .expect("Unable to get client");
+    let api: Api<Service> = Api::default_namespaced(client);
+
+    let valid: Service = serde_yaml::from_str(VALID_MANIFEST).unwrap();
+    // Create a good service first
+    api.create(&PostParams::default(), &valid)
+        .await
+        .expect("Should be able to create valid service");
+
+    // The test scaffolding doesn't wait for an ack from the link, so wait for a bit
+    tokio::time::sleep(std::time::Duration::from_secs(3)).await;
+
+    let actor_id = prov.origin().public_key();
+    // create client and ctx
+    let client = KubernetesApplierSender::via(prov);
+    let ctx = Context {
+        actor: Some(actor_id),
+        ..Default::default()
+    };
+
+    let resp = client
+        .apply(&ctx, &INVALID_UPDATE_MANIFEST.as_bytes().to_vec())
+        .await?;
+    assert!(!resp.succeeded, "Update should not have succeeded");
+    assert!(resp.error.is_some(), "Error message should be set");
+
+    Ok(())
+}
+
+/// Test that a non-existent delete succeeds
+async fn nonexistent_delete(_opt: &TestOptions) -> RpcResult<()> {
+    let prov = test_provider().await;
+    let svc_name = "foo-applier-test-noexist";
+
+    // The test scaffolding doesn't wait for an ack from the link, so wait for a bit
+    tokio::time::sleep(std::time::Duration::from_secs(3)).await;
+
+    let actor_id = prov.origin().public_key();
+    // create client and ctx
+    let client = KubernetesApplierSender::via(prov);
+    let ctx = Context {
+        actor: Some(actor_id),
+        ..Default::default()
+    };
+
+    let resp = client
+        .delete(
+            &ctx,
+            &DeleteRequest {
+                group: String::new(),
+                kind: "Service".into(),
+                version: "v1".into(),
+                name: svc_name.into(),
+                ..Default::default()
+            },
+        )
+        .await?;
+    assert!(resp.succeeded, "Delete should have succeeded");
 
     Ok(())
 }
