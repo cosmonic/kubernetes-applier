14116974a4aadd27cfe27967819813ca2562ee32
Taylor Thomas <taylor@cosmonic.com>
2022-03-07 15:36:25 -0700

    chore(*): Updates all code to the latest dependency versions

--- applier/src/main.rs
+++ applier/src/main.rs
@@ -32,6 +32,11 @@ const FIELD_MANAGER: &str = "kubernetes-applier-provider";
 // and returns only when it receives a shutdown message
 //
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt()
+        .with_writer(std::io::stderr)
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .with_ansi(atty::is(atty::Stream::Stderr))
+        .init();
     info!("Starting provider process");
     provider_main(ApplierProvider::default())?;
 
